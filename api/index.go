7162d7ff7f3e5a715e79602164649d75592f02fc
Nazeer Ali <nazeer@example.com>
2026-08-16 17:26:59 +0530

    Configure Vercel serverless deployment for nazeer.nx.kg

---
+++ api/index.go
@@ -0,0 +1,59 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/yigithankarabulut/distributed-file-storage/config"
+	"github.com/yigithankarabulut/distributed-file-storage/crypto"
+	"github.com/yigithankarabulut/distributed-file-storage/db"
+	"github.com/yigithankarabulut/distributed-file-storage/fileserver"
+	"github.com/yigithankarabulut/distributed-file-storage/p2p"
+	"github.com/yigithankarabulut/distributed-file-storage/store"
+)
+
+var (
+	appInstance *App
+	muxInstance *http.ServeMux
+)
+
+func init() {
+	cfg := config.LoadConfig()
+	encryptKey, _ := crypto.NewEncryptionKey()
+
+	gdriveStore := store.NewGDriveStore("credentials.json", store.DefaultVaultFolderName)
+
+	s1 := makeServerNode("3000_network", encryptKey, gdriveStore)
+	s2 := makeServerNode("4000_network", encryptKey, gdriveStore)
+	s3 := makeServerNode("5000_network", encryptKey, gdriveStore)
+
+	servers := []*fileserver.FileServer{s1, s2, s3}
+	database := db.Connect(cfg)
+
+	appInstance = New(servers, database)
+	muxInstance = http.NewServeMux()
+	RegisterRoutes(muxInstance, appInstance)
+}
+
+func makeServerNode(storageRoot string, encryptKey []byte, gdriveStore *store.GDriveStore) *fileserver.FileServer {
+	tcpTransport := p2p.NewTCPTransport(
+		p2p.WithListenAddr(":0"),
+		p2p.WithHandshakeFunc(p2p.NOPHandshakeFunc),
+		p2p.WithDecoder(&p2p.DefaultDecoder{}),
+	)
+
+	fileServerOpts := fileserver.ServerOpts{
+		EncryptKey:        encryptKey,
+		StorageRoot:       storageRoot,
+		PathTransformFunc: store.CASPathTransformFunc,
+		Transport:         tcpTransport,
+	}
+
+	srv := fileserver.NewFileServer(fileServerOpts)
+	srv.GDrive = gdriveStore
+	return srv
+}
+
+// Handler is the Vercel serverless entry point.
+func Handler(w http.ResponseWriter, r *http.Request) {
+	muxInstance.ServeHTTP(w, r)
+}
